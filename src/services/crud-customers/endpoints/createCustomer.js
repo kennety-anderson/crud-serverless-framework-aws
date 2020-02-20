56a39244609afa693ee328d8f7586a3b5323c272
Kennety Anderson <kennetyg79@gmail.com>
2020-02-20 00:24:53 -0300

    refactor para o cors

--- src/services/crud-customers/endpoints/createCustomer.js
+++ src/services/crud-customers/endpoints/createCustomer.js
@@ -1,53 +1,53 @@
-'use strict'
+"use strict";
 
-const AWS = require('aws-sdk')
-const middy = require('@middy/core')
-const doNotWaitForEmptyEventLoop = require('@middy/do-not-wait-for-empty-event-loop')
-const warmup = require('@middy/warmup')
-const cors = require('@middy/http-cors')
-const httpErrorHandler = require('@middy/http-error-handler')
-const createError = require('http-errors')
-const { createConnection } = require('../../../database/mongo/connection')
-const Customer = require('../../../database/mongo/models/Customer')
+const AWS = require("aws-sdk");
+const middy = require("@middy/core");
+const doNotWaitForEmptyEventLoop = require("@middy/do-not-wait-for-empty-event-loop");
+const warmup = require("@middy/warmup");
+const cors = require("@middy/http-cors");
+const httpErrorHandler = require("@middy/http-error-handler");
+const createError = require("http-errors");
+const { createConnection } = require("../../../database/mongo/connection");
+const Customer = require("../../../database/mongo/models/Customer");
 
-const sns = new AWS.SNS()
+const sns = new AWS.SNS();
 
 const handler = middy(async (event, context) => {
-  const body = JSON.parse(event.body)
+  const body = JSON.parse(event.body);
 
   try {
-    await createConnection() // criando conexão com o banco de dados
+    await createConnection(); // criando conexão com o banco de dados
 
-    const data = await Customer.create(body)
+    const data = await Customer.create(body);
 
     const params = {
       Message: JSON.stringify(data),
       TopicArn: process.env.SNS_TOPIC_CUSTOMER_CREATE
-    }
+    };
 
-    await sns.publish(params).promise()
+    await sns.publish(params).promise();
 
     return {
       statusCode: 200,
       body: JSON.stringify({
         _id: data._id
       })
-    }
+    };
   } catch (err) {
     // erifica se é um erro de validação para dar uma resposta persoanlizada ao cliente
-    if (err.name === 'ValidationError') {
-      throw createError(400, 'Erro de validação verifique os campos enviados')
+    if (err.name === "ValidationError") {
+      throw createError(400, "Erro de validação verifique os campos enviados");
     }
 
     // criação de um erro de requisição mal formada
-    throw createError(400)
+    throw createError(400);
   }
-})
+});
 
 handler
   .use(doNotWaitForEmptyEventLoop()) // adiciona o context.doNotWaitForEmptyEventLoop = false
   .use(warmup({ waitForEmptyEventLoop: false })) // retorna de forma rapida quando é um evento warmup
-  .use(cors()) // adiciona os headers do cors (tem que ser antes do response)
   .use(httpErrorHandler()) // valida qualquer erro do formato http-errors
+  .use(cors()); // adiciona os headers do cors (tem que ser antes do response)
 
-module.exports = { handler }
+module.exports = { handler };
--- src/services/crud-customers/endpoints/findOneCustomer.js
+++ src/services/crud-customers/endpoints/findOneCustomer.js
@@ -1,40 +1,40 @@
-const middy = require('@middy/core')
-const doNotWaitForEmptyEventLoop = require('@middy/do-not-wait-for-empty-event-loop')
-const warmup = require('@middy/warmup')
-const cors = require('@middy/http-cors')
-const httpErroHandler = require('@middy/http-error-handler')
-const createError = require('http-errors')
-const { ObjectId } = require('mongodb')
-const { createConnection } = require('../../../database/mongo/connection')
-const Customer = require('../../../database/mongo/models/Customer')
+const middy = require("@middy/core");
+const doNotWaitForEmptyEventLoop = require("@middy/do-not-wait-for-empty-event-loop");
+const warmup = require("@middy/warmup");
+const cors = require("@middy/http-cors");
+const httpErroHandler = require("@middy/http-error-handler");
+const createError = require("http-errors");
+const { ObjectId } = require("mongodb");
+const { createConnection } = require("../../../database/mongo/connection");
+const Customer = require("../../../database/mongo/models/Customer");
 
 const handler = middy(async (event, context) => {
-  const { id } = event.pathParameters
+  const { id } = event.pathParameters;
 
   try {
-    await createConnection()
+    await createConnection();
 
-    if (!ObjectId.isValid(id)) throw createError(422, 'ID malformed')
+    if (!ObjectId.isValid(id)) throw createError(422, "ID malformed");
 
-    const data = await Customer.findById(id)
+    const data = await Customer.findById(id);
 
-    if (!data) throw createError(404, 'Customer is not found!')
+    if (!data) throw createError(404, "Customer is not found!");
 
     return {
       statusCode: 200,
       body: JSON.stringify({
         user: data
       })
-    }
+    };
   } catch (err) {
-    throw createError(err.statusCode, err.message)
+    throw createError(err.statusCode, err.message);
   }
-})
+});
 
 handler
   .use(doNotWaitForEmptyEventLoop()) // adiciona o context.doNotWaitForEmptyEventLoop = false
   .use(warmup({ waitForEmptyEventLoop: false })) // retorna de forma rapida quando é um evento warmup
-  .use(cors()) // adiciona os headers do cors (tem que ser antes do response)
   .use(httpErroHandler()) // valida qualquer erro do formato http-errors
+  .use(cors()); // adiciona os headers do cors (tem que ser antes do response)
 
-module.exports = { handler }
+module.exports = { handler };
