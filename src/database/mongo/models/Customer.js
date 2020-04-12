48e19d16a52c4ed1eab3487fc6327fd53db41486
kennety Anderson <kennetyg79@gmail.com>
2020-04-11 23:58:34 -0300

    refatoração de testes unitarios

--- src/database/mongo/models/Customer.js
+++ src/database/mongo/models/Customer.js
@@ -1,38 +1,38 @@
-const { Schema, model, connection } = require("mongoose");
-const bcrypt = require("bcryptjs");
+const { Schema, model, connection } = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 // schema de model
 const CustomerSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, "E necessario um nome de usuario"]
+      required: [true, 'E necessario um nome de usuario']
     },
     email: {
       type: String,
-      required: [true, "E necessario um email unico de usuario"],
+      required: [true, 'E necessario um email unico de usuario'],
       unique: true
     },
     cpf: {
       type: String,
-      required: [true, "E necessario um cpf"],
+      required: [true, 'E necessario um cpf'],
       maxlength: 11,
       minlength: 11,
       unique: true
     },
     birthdate: {
       type: String,
-      required: [true, "E necessaria a data de nascimento"]
+      required: [true, 'E necessaria a data de nascimento']
     },
     password: {
       type: String,
-      required: [true, "E necessaria a escolha de uma senha"],
+      required: [true, 'E necessaria a escolha de uma senha'],
       minlength: 8
     },
     permission: {
       type: String,
-      default: "CUSTOMER",
-      enum: ["CUSTOMER", "USER", "ADMIN"]
+      default: 'CUSTOMER',
+      enum: ['CUSTOMER', 'USER', 'ADMIN']
     },
     avatar: {
       type: String,
@@ -46,34 +46,34 @@ const CustomerSchema = new Schema(
   {
     timestamps: true
   }
-);
+)
 
 // faz com que nunca retorne o password nas operações
-CustomerSchema.methods.toJSON = function() {
-  let customerObject = this.toObject();
-  delete customerObject.password;
+CustomerSchema.methods.toJSON = function () {
+  const customerObject = this.toObject()
+  delete customerObject.password
 
-  return customerObject;
-};
+  return customerObject
+}
 
 // encriptação antes de salvar na database
-CustomerSchema.pre("save", async function(next) {
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
+CustomerSchema.pre('save', async function (next) {
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
+})
 
 // encriptação ao atualizar
-CustomerSchema.pre("findOneAndUpdate", async function(next) {
-  if (!this._update) next();
+CustomerSchema.pre('findOneAndUpdate', async function (next) {
+  if (!this._update) next()
 
   if (this._update.password) {
-    this._update.password = await bcrypt.hash(this._update.password, 10);
+    this._update.password = await bcrypt.hash(this._update.password, 10)
   }
-  next();
-});
+  next()
+})
 
 // verificação se esta rodando offline para executar um delete do model
-if (process.env.IS_OFFLINE) delete connection.models.Customer;
+if (process.env.IS_OFFLINE) delete connection.models.Customer
 
 // export do model
-module.exports = model("Customer", CustomerSchema);
+module.exports = model('Customer', CustomerSchema)
--- src/services/crud-customers/__tests__/createCustomer.test.js
+++ src/services/crud-customers/__tests__/createCustomer.test.js
@@ -1,87 +1,87 @@
 // requires do test
-const mongoose = require('mongoose')
-const mockingoose = require('mockingoose').default
-const { createConnection } = require('../../../database/mongo/connection')
-const { handler } = require('../endpoints/createCustomer')
-const Customer = require('../../../database/mongo/models/Customer')
+const mongoose = require("mongoose");
+const mockingoose = require("mockingoose").default;
+const { createConnection } = require("../../../database/mongo/connection");
+const { handler } = require("../endpoints/createCustomer");
+const Customer = require("../../../database/mongo/models/Customer");
 
 // mock opcional pois require do mockingoose ja mock a conexão
-jest.mock('../../../database/mongo/connection')
+jest.mock("../../../database/mongo/connection");
 
-jest.mock('aws-sdk', () => {
+jest.mock("aws-sdk", () => {
   return {
     SNS: function () {
       return {
         publish: () => {
           return {
-            promise: () => {}
-          }
-        }
-      }
-    }
-  }
-})
+            promise: () => {},
+          };
+        },
+      };
+    },
+  };
+});
 
-describe('Create customer:', () => {
-  const _id = new mongoose.Types.ObjectId().toHexString()
+describe("Create customer:", () => {
+  const _id = new mongoose.Types.ObjectId().toHexString();
   const customer = {
     _id,
-    name: 'kenny',
-    email: 'kem@gmail.com',
-    cpf: '01234567891',
-    password: '12345678',
-    birthDate: '2000-01-01'
-  } // exemplo de um objeto de cadastro de customer
+    name: "kenny",
+    email: "kem@gmail.com",
+    cpf: "01234567891",
+    password: "12345678",
+    birthdate: "2000-01-01",
+  }; // exemplo de um objeto de cadastro de customer
 
-  const context = {}
+  const context = {};
 
   beforeEach(() => {
-    jest.restoreAllMocks()
-  })
+    jest.restoreAllMocks();
+  });
 
   beforeAll(() => {
-    createConnection.mockImplementation(() => Promise.resolve())
-  })
+    createConnection.mockImplementation(() => Promise.resolve());
+  });
 
-  it('verify customer ceated successfully:', async done => {
-    mockingoose(Customer).toReturn(customer, 'save') // simula o metodo save do customer
-    const event = { body: JSON.stringify(customer) }
+  it("verify customer ceated successfully:", async (done) => {
+    mockingoose(Customer).toReturn(customer, "save"); // simula o metodo save do customer
+    const event = { body: JSON.stringify(customer) };
 
-    const result = await handler(event, context)
+    const result = await handler(event, context);
 
-    expect(result).toHaveProperty('statusCode', 200)
-    expect(result).toHaveProperty('body')
-    expect(JSON.parse(result.body)).toHaveProperty('_id')
+    expect(result).toHaveProperty("statusCode", 200);
+    expect(result).toHaveProperty("body");
+    expect(JSON.parse(result.body)).toHaveProperty("data");
 
-    done()
-  })
+    done();
+  });
 
-  it('testing creation driver error', async done => {
-    const context = {}
-    const event = { body: JSON.stringify(customer) } // sem stringify
+  it("testing creation driver error", async (done) => {
+    const context = {};
+    const event = { body: JSON.stringify(customer) }; // sem stringify
 
-    const err = new mongoose.Error.ValidationError()
+    const err = new mongoose.Error.ValidationError();
 
-    mockingoose(Customer).toReturn(err, 'save')
+    mockingoose(Customer).toReturn(err, "save");
 
-    const result = await handler(event, context)
+    const result = await handler(event, context);
 
-    expect(result).toHaveProperty('statusCode', 400)
-    expect(result).toHaveProperty('body')
-    done()
-  })
+    expect(result).toHaveProperty("statusCode", 400);
+    expect(result).toHaveProperty("body");
+    done();
+  });
 
-  it('create customer error', async done => {
-    const event = { body: JSON.stringify(customer) }
+  it("create customer error", async (done) => {
+    const event = { body: JSON.stringify(customer) };
 
-    mockingoose(Customer).toReturn(new Error('Timeout'), 'save')
+    mockingoose(Customer).toReturn(new Error("Timeout"), "save");
 
     try {
-      await handler(event, context)
+      await handler(event, context);
     } catch (err) {
-      expect(err.message).toEqual('Timeout')
+      expect(err.message).toEqual("Timeout");
     }
 
-    done()
-  })
-})
+    done();
+  });
+});
